39a7a931bb239702803cb0986673e7d97c01ff8f
Gabriel <Gabriel@Gabriel-Lap>
2015-08-29 11:34:35 -0600

    Primeros subidos

---
+++ src/server2/main.java
@@ -0,0 +1,14 @@
+
+package server2;
+
+import interfaz.MENU.GUI;
+
+public class main {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        GUI window = new GUI ();
+        window.setVisible(true);
+    }
+    
+}
