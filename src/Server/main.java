e5652cded4398a6a69126eacc87af92e5eaa1970
Gabriel <Gabriel@Gabriel-Lap>
2015-08-29 13:52:16 -0600

    Primeros subidos renombrados

--- src/Server/main.java
+++ src/Server/main.java
@@ -1,14 +1,11 @@
 
 package server2;
 
-import interfaz.MENU.GUI;
 
 public class main {
 
     public static void main(String[] args) {
         // TODO code application logic here
-        GUI window = new GUI ();
-        window.setVisible(true);
     }
     
 }
