c12de0ef1d63a2dca18cedef356b4a59b207a690
Gabriel <Gabriel@Gabriel-Lap>
2015-09-10 17:27:54 -0600

    Acciones

---
+++ Actions/Time.java
@@ -0,0 +1,24 @@
+package Actions;
+
+public class Time {
+
+	public static void main(String[] args) {
+		int x = 3;
+		int minutos=x-1;
+		int segundos = 60;
+		for(minutos=minutos;minutos>-1;minutos--){
+			for(segundos=60;segundos>-1;segundos--){
+				System.out.println(minutos+":"+segundos);
+				delaySegundo();
+			}
+		}
+
+	}
+	private static void delaySegundo(){
+		try{
+			Thread.sleep(1000);
+		}
+		catch(InterruptedException e){}
+	}
+
+}
